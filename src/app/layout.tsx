6b4e9e6f7ed24f34ff6a794af8fb5e740e702fca
Dave <118544840+disfordave@users.noreply.github.com>
2025-10-07 23:08:47 +0900

    feat: update RootLayout to include dynamic WIKI_NAME link and restructure Home component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { WIKI_NAME } from "@/lib/config";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.className} ${geistMono.variable} antialiased bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100`}
       >
-        {children}
+        <div className="min-h-screen p-4 mx-auto max-w-2xl">
+          <h1 className="text-3xl font-bold"><Link href={'/'}>{WIKI_NAME}</Link></h1>
+          {children}
+        </div>
       </body>
     </html>
   );
