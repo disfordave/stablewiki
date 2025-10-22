df3ec54d6a0c057730e9a6927f8f0af53cb8a59b
Dave <118544840+disfordave@users.noreply.github.com>
2025-10-22 18:23:32 +0900

    feat: disable user signup endpoint and update signup page to reflect demo status

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -7,79 +7,83 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { username, password } = body;
 
-  if (!username || !password) {
-    return Response.json(
-      { error: "Username and password are required" },
-      { status: 400 }
-    );
-  }
+  return Response.json({
+    error: "The endpoint has been disabled."
+  }, { status: 403 });
 
-  try {
-    const existingUser = await prisma.user.findUnique({
-      where: { username: username },
-    });
+  // if (!username || !password) {
+  //   return Response.json(
+  //     { error: "Username and password are required" },
+  //     { status: 400 }
+  //   );
+  // }
 
-    if (existingUser) {
-      return Response.json(
-        { error: "Username already taken" },
-        { status: 409 }
-      );
-    }
+  // try {
+  //   const existingUser = await prisma.user.findUnique({
+  //     where: { username: username },
+  //   });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  //   if (existingUser) {
+  //     return Response.json(
+  //       { error: "Username already taken" },
+  //       { status: 409 }
+  //     );
+  //   }
 
-    const newUser = await prisma.user.create({
-      data: {
-        username: username,
-        password: hashedPassword,
-        role: "USER", // Default role
-      },
-    });
+  //   const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (!process.env.JWT_SECRET) {
-      console.error("JWT_SECRET is not defined in the environment variables.");
-      return Response.json({ error: "Internal server error" }, { status: 500 });
-    }
+  //   const newUser = await prisma.user.create({
+  //     data: {
+  //       username: username,
+  //       password: hashedPassword,
+  //       role: "USER", // Default role
+  //     },
+  //   });
 
-    let token;
-    try {
-      //Creating jwt token
-      token = jwt.sign(
-        {
-          id: newUser.id,
-          username: newUser.username,
-          avatarUrl: newUser.avatarUrl,
-          role: newUser.role,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "1h" }
-      );
-    } catch (err) {
-      console.log(err);
-      return Response.json(
-        { error: "Failed to sign up user from JWT" },
-        { status: 500 }
-      );
-    }
+  //   if (!process.env.JWT_SECRET) {
+  //     console.error("JWT_SECRET is not defined in the environment variables.");
+  //     return Response.json({ error: "Internal server error" }, { status: 500 });
+  //   }
 
-    const response = Response.json({
-      message: "Signup successful! Welcome aboard!",
-      user: { id: newUser.id, username: newUser.username, avatarUrl: newUser.avatarUrl, role: newUser.role, token },
-    });
+  //   let token;
+  //   try {
+  //     //Creating jwt token
+  //     token = jwt.sign(
+  //       {
+  //         id: newUser.id,
+  //         username: newUser.username,
+  //         avatarUrl: newUser.avatarUrl,
+  //         role: newUser.role,
+  //       },
+  //       process.env.JWT_SECRET,
+  //       { expiresIn: "1h" }
+  //     );
+  //   } catch (err) {
+  //     console.log(err);
+  //     return Response.json(
+  //       { error: "Failed to sign up user from JWT" },
+  //       { status: 500 }
+  //     );
+  //   }
 
-    const cookieStore = await cookies();
-    cookieStore.set({
-      name: "jwt",
-      value: token,
-      httpOnly: true,
-      sameSite: "lax",
-      path: "/",
-      maxAge: 3600,
-    });
+  //   const response = Response.json({
+  //     message: "Signup successful! Welcome aboard!",
+  //     user: { id: newUser.id, username: newUser.username, avatarUrl: newUser.avatarUrl, role: newUser.role, token },
+  //   });
 
-    return response;
-  } catch (error) {
-    console.error(error);
-    return Response.json({ error: "Failed to sign up user" }, { status: 500 });
-  }
+  //   const cookieStore = await cookies();
+  //   cookieStore.set({
+  //     name: "jwt",
+  //     value: token,
+  //     httpOnly: true,
+  //     sameSite: "lax",
+  //     path: "/",
+  //     maxAge: 3600,
+  //   });
+
+  //   return response;
+  // } catch (error) {
+  //   console.error(error);
+  //   return Response.json({ error: "Failed to sign up user" }, { status: 500 });
+  // }
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,36 +1,31 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export default function SignupPage() {
-    async function handleSignup(formData: FormData) {
-        "use server"
-        const username = formData.get("username")?.toString() || "";
-        const password = formData.get("password")?.toString() || "";
+    // async function handleSignup(formData: FormData) {
+    //     "use server"
+    //     const username = formData.get("username")?.toString() || "";
+    //     const password = formData.get("password")?.toString() || "";
 
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/signup`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ username, password }),
-        });
+    //     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/signup`, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({ username, password }),
+    //     });
 
-        if (res.ok) {
-            // Redirect to signin page with success message
-            redirect(`/`);
-        } else {
-            const data = await res.json();
-            redirect(`/signup?error=${encodeURIComponent(data.error || "An unexpected error occurred")}`);
-        }
-    }
+    //     if (res.ok) {
+    //         // Redirect to signin page with success message
+    //         redirect(`/`);
+    //     } else {
+    //         const data = await res.json();
+    //         redirect(`/signup?error=${encodeURIComponent(data.error || "An unexpected error occurred")}`);
+    //     }
+    // }
     return (
         <div>
         <h1>Sign Up</h1>
-        <form action={handleSignup}>
-            <input type="text" name="username" placeholder="Username" required />
-            <input type="password" name="password" placeholder="Password" required />
-            <button type="submit">Sign Up</button>
-            <p className="text-red-500">{/* Display error message if any */}</p>
-        </form>
+        <p>This is a demo project, therefore user creation is disabled.</p>
         </div>
     )
 }
