e11b898305ce01e4672400507b75daf3c632da75
Dave <118544840+disfordave@users.noreply.github.com>
2025-10-23 16:37:46 +0900

    Update Footer.tsx, expanding footer y axis padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { WIKI_COPYRIGHT_HOLDER, WIKI_COPYRIGHT_HOLDER_URL } from "@/config";
 
 export default function Footer() {
   return (
-    <footer className="rounded-t-2xl bg-white p-4 text-center sm:rounded-2xl dark:bg-gray-800">
+    <footer className="rounded-t-2xl bg-white px-4 py-8 text-center sm:rounded-2xl dark:bg-gray-800">
       <p className="text-sm text-gray-500">
         &copy; {new Date().getFullYear()}{" "}
         <a
