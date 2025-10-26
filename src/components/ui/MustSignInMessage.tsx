b8bf2cd148e40105983869cf694b27f89d0ce07c
Dave <118544840+disfordave@users.noreply.github.com>
2025-10-26 12:04:49 +0900

    Add GPLv3 license header to all source files

--- src/components/ui/MustSignInMessage.tsx
+++ src/components/ui/MustSignInMessage.tsx
@@ -1,3 +1,23 @@
+/*
+    StableWiki is a modern, open-source wiki platform focused on simplicity,
+    collaboration, and ease of use.
+
+    Copyright (C) 2025 @disfordave
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/solid";
 import { TransitionLinkButton } from "./TransitionButton";
 
