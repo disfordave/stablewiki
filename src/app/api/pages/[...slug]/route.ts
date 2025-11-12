72da27eadbd368e20356a138a2a11c0577c08ecb
Dave <118544840+disfordave@users.noreply.github.com>
2025-11-12 18:12:03 +0900

    Update license to GNU Affero General Public License 3.0 and later

--- src/app/api/pages/[...slug]/route.ts
+++ src/app/api/pages/[...slug]/route.ts
@@ -5,16 +5,16 @@
     Copyright (C) 2025 @disfordave
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
@@ -314,6 +314,7 @@ export async function DELETE(
   { params }: { params: Promise<{ slug: string[] }> },
 ) {
   const { slug } = await params;
+  // const isMediaPage = slug[0].split(":")[0].toLowerCase() === "media";
 
   const decodedToken = await getDecodedToken(request);
 
