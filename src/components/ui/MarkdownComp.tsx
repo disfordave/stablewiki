cceb391feea20e8d5283300164f005f31e5c7647
Dave <118544840+disfordave@users.noreply.github.com>
2025-11-29 19:30:05 +0900

    style: update Markdown component styles for improved layout and appearance

--- src/components/ui/MarkdownComp.tsx
+++ src/components/ui/MarkdownComp.tsx
@@ -69,7 +69,7 @@ export function WikiMarkdown({
             <Image
               className={
                 (className ? className + " " : "") +
-                "my-[2em] w-full rounded-xl"
+                "mt-4 mb-4 max-h-[61vh] w-full rounded-xl bg-gray-100 object-contain dark:bg-gray-900"
               }
               width={0}
               height={0}
@@ -89,7 +89,7 @@ export function WikiMarkdown({
           const { children, className, node, ...rest } = props;
           if (className === "toc" && !isComment) {
             return (
-              <details className="-mb-2 rounded-xl bg-gray-100 p-4 dark:bg-gray-900">
+              <details className="rounded-xl bg-gray-100 p-4 dark:bg-gray-900">
                 <summary className="-m-4 p-4 font-bold select-none">
                   Contents
                 </summary>
